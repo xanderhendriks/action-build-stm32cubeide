fda54c0101fed252c2a53af936709198467873c0
Xander Hendriks <xander.hendriks@nx-solutions.com>
2021-02-07 20:10:03 +1100

    Some debugging commands in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
-
+ls /opt/**
+which stm32cubeide
 stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data workspace -import $1
 headless-build.sh -data workspace -build $2
