4aac018fa7ad64eb662ab3737aaddd6b1f8a0f3b
Xander Hendriks <xander.hendriks@nx-solutions.com>
2022-02-03 11:24:33 +1100

    Add an alias for Python3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+alias python='python3'
 stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data workspace -import $1
 headless-build.sh -data workspace -build $2
