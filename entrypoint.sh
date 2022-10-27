6c4d7f6041d5ae70028b234ed073ab7c8f13ac08
Xander Hendriks <57311059+xanderhendriks@users.noreply.github.com>
2022-10-27 19:06:44 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data workspace -import $1
+stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -import $1
 headless-build.sh -data workspace -build $2
