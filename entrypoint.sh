9c766d2c448a092272916871d7f2feb927b53262
addshore <addshorewiki@gmail.com>
2024-04-12 15:57:06 +0100

    Allow additional args to headless-build.sh
    THis allows passing in extra defines etc when
    used via github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -import "$1"
-headless-build.sh -data /tmp/stm-workspace -build "$2"
+headless-build.sh -data /tmp/stm-workspace -build "$2" "${@:3}"
