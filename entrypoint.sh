4e2c784f06ff38d148cabe1c92c6ac66f7f506c2
Xander Hendriks <xander@nx-solutions.com>
2025-03-17 15:34:32 +1100

    Using importAll instead import upon Dominik Wetzel's request

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -import "$1"
+stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -importAll "$1"
 headless-build.sh -data /tmp/stm-workspace -build "$2"
